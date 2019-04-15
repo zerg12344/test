c226defa83daf4d9b79c7164088643b3ebd2c04e
Sergey <sergey@netforia.ru>
2019-04-15 13:16:12 +0300

    это наш первый комит

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*//null;
+undefined;
+
+
+let person ={
+
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person.age);
+
+var string = "Number";
+
+console.log(string);
+console.log(person.name);*/
+
+
+//alert("пошел");
+
+//let answer = confirm("are you here?");
+
+//console.log(answer);
+
+//let answer = prompt("are you here?", "");
+
+//console.log(answer);
+
+
+let incr =10,
+    decr =10;
+
+     console.log(++incr);
+     console.log(--decr);
+    console.log ("2" == 3);
